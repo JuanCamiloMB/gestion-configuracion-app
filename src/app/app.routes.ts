4315317a268af37725dc583f974256fe85e783bb
JuanK <juancamilo.manjarres@gmail.com>
2024-03-20 16:46:05 -0500

    feat: Routing added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { NoteListComponent } from './note-list/note-list.component';
+import { NoteEditComponent } from './note-edit/note-edit.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
+  {
+    path: 'notelist',
+    component: NoteListComponent,
+  },
+  {
+    path: 'noteedit',
+    component: NoteEditComponent,
+  },
+];
