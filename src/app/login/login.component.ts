592c87b1cef60a003c9ef2e5ae929398faef71a8
JuanK <juancamilo.manjarres@gmail.com>
2024-03-26 11:08:04 -0500

    feat: form values

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm = new FormGroup({
+    passwordControl: new FormControl(''),
+    usernameControl: new FormControl(''),
+  });
 
+  onSubmit(){
+    const { usernameControl: username, passwordControl: password } =
+      this.loginForm.value;
+
+    console.log(username, password)
+  }
+  
 }
