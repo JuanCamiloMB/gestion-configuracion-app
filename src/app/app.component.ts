ad3bd410f6b4c9ecf7f2e959923dea61838ac064
redundante3452 <ahernandez429@soyudemedellin.edu.co>
2024-03-27 14:14:41 -0500

    login y registro terminados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { BannerComponent } from './banner/banner.component';
+import { SignupComponent } from './signup/signup.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, BannerComponent],
+  imports: [RouterOutlet, LoginComponent, BannerComponent,SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
