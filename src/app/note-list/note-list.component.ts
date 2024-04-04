2a07c62d3881795f0e93333bacee5fec1527bb3c
JuanK <juancamilo.manjarres@gmail.com>
2024-04-04 11:04:13 -0500

    feat: List notes

--- src/app/note-list/note-list.component.ts
+++ src/app/note-list/note-list.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../services/user.service';
+import { FirestoreService } from '../services/firestore.service';
 
 @Component({
   selector: 'app-note-list',
   standalone: true,
   imports: [],
   templateUrl: './note-list.component.html',
-  styleUrl: './note-list.component.css'
+  styleUrl: './note-list.component.css',
 })
 export class NoteListComponent {
+  noteList: any;
+  userData: any;
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private _userService: UserService,
+    private _firestoreService: FirestoreService
+  ) {
+    this._userService.getUserData();
+    this.userData = this._userService.userData;
+  }
 
+  getNotes() {
+    const username = this.userData.username;
+    this.noteList = this._firestoreService.getNotes(username);
+  }
 }
